483c25f96023ddd24d83b2c64dd7bdfe04d6f8b5
pry0cc <pry0cc@protonmail.com>
2017-09-25 20:51:13 +0000

    Added build

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 #docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ruby:2.1 bundle install
 docker build -t pry0cc/leaderboard .
+docker rm -f leaderboard
 docker run -d -p 80:8080 --name leaderboard pry0cc/leaderboard
+
